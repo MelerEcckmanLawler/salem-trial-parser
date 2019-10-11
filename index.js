b3e8c2585fd87278466dfd74d45016d26b6757e1
MelerEcckmanLawler <34365491+MelerEcckmanLawler@users.noreply.github.com>
2019-10-11 05:32:38 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const cheerio = require('cheerio')
 const fs = require('fs').promises
 
+parseReport(2505930)
+//2501835
+//2504760
+//2504771
+//2505868
+//2505930
+
 async function reportToString(reportId) {
   let HTML = await fs.readFile(`${reportId}.html`, `utf8`)
   return HTML
@@ -41,47 +48,6 @@ function getSpans(HTML) {
 }
 
 function spansToArrayOfEntries(spans, players) {
-  // { data: 'Lobby', attribs: { class: 'stage' } }
-  // { data: 'Players are choosing names', attribs: { class: 'stage' } }
-  // { data: 'Ranked Game.', attribs: { title: '  ', class: 'notice' } }
-
-  // CHAT MESSAGE
-  // { data: 'Fancy Dube: Hi', attribs: { title: 'Godfather2', class: 'DonParme Godfather2' } }
-  // { data: 'Day 1', attribs: { class: 'time day' } }
-  // { data: 'Night 2', attribs: { class: 'time night' } }
-
-  // JAIL CHAT
-  // { data: 'Edward Bishop: Hey' ,attribs: { title: 'Godfather', class: 'OliUnglaube007 Godfather jail' } }
-
-  //Godfather2 is an actual role! So is Investigator2, etc.
-
-  // MAFIA CHAT
-  // { data: 'William Hobbs: im escort', attribs: { title: 'Mafioso2', class: 'aglayin123 Mafioso2 mafia' } }
-
-  // ABILITY
-  // { data: 'flame investigated Spheal.',attribs: { title: 'Investigator  ', class: 'notice Investigator  Investigator' } }
-
-  // NIGHT KILL
-  // { data: 'Court has been killed.', attribs: { title: 'Medium  ', class: 'notice Medium  death' } }
-  // { data: 'Court was attacked by the Mafia.', attribs: { title: 'Medium  ', class: 'notice' } }
-
-  // LAST WILL / DEATH NOTE
-  // { data: undefined, attribs: { class: 'note','data-type': 'will', 'data-info': 'PHNwYW4gaWQ9J3dpbGxPd25lcic+U3BoZWFsKFF1YWtlTkxEKTo8L3NwYW4+U3BoZWFsJm5ic3A7LSZuYnNwO0ludmVzdGlnYXRvcjxiciAvPg08YnIgLz4NTjE6VmlnaWwmbmJzcDsoMTQpPGJyIC8+' } }
-
-  // DEAD CHAT
-  // { data: 'Spheal: Ok Medium, straight down to business: Spheal - InvestigatorN1: Vigil (14) - Lookout, Forger, Witch', attribs: { title: 'Investigator', class: 'QuakeNLD Investigator dead' } }
-
-  // MEDIUM
-  // { data: 'Court: Medium Court here', attribs: { title: 'Medium', class: 'ClainS Medium seance' } }
-
-  // WHISPER
-  // { data: 'Edward Bishop to Simp:  well u fucked up ur fault ', attribs: { title: 'Godfather Jester OliUnglaube007 Badream ', class: 'Godfather Jester OliUnglaube007 Badream whisper' } }
-
-  // TRANSPORT
-  // { data: 'Transporter swapped Clipper with Edward Bishop.',attribs: { title: 'Veteran Godfather ', class: 'notice Veteran Godfather Transporter trans' } }
-
-  // END
-  // { data: 'End of Report', attribs: { class: 'end' } }
   for (let k in players) {
     console.log(players[k].name, players[k].role)
   }
@@ -596,10 +562,3 @@ async function parseReport(reportId) {
   let spans = getSpans(HTML)
   spans = spansToArrayOfEntries(spans, players)
 }
-
-//2501835
-//2504760
-//2504771
-//2505868
-//2505930
-parseReport(2505930)
